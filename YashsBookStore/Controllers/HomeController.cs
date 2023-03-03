42d3e4d59906e99f01aad22ffd03c074ee31c3c9
Yash Rameshbhai Kheni <W0816059@myscc.ca>
2023-03-03 16:11:48 -0500

    >readme file updated >new theme has been applied.

--- YashsBookStore/Controllers/HomeController.cs
+++ YashsBookStore/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace YashsBookStore.Controllers
 
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
+            _logger = logger; 
         }
 
         public IActionResult Index()
